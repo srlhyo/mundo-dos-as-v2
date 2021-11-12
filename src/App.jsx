77ef08bc2198281abcefb3ff53c936f486deb1a4
helio@weathersafe.co.uk <helioschultz_1@msn.com>
2021-11-12 08:49:51 +0000

    navigation bar first draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from "./images/logo-small.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-blue-600">Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div>
+      <header>
+        <img src={logo} alt="Logo" />
+        <nav>
+          <ul>
+            <li>home</li>
+            <li>sobre nos</li>
+            <li>galeria</li>
+            <li>contacto</li>
+          </ul>
+        </nav>
       </header>
     </div>
   )
