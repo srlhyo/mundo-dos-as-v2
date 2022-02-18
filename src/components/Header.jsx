f37b3470487e446bf5860296efe76d1f1d511072
helio@weathersafe.co.uk <helioschultz_1@msn.com>
2022-02-18 13:13:15 +0000

    another merge conflict fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,12 +29,7 @@ function Header() {
         <div className="self-start pt-10 sm:pt-16 text-center" id="Home">
           <Heading heading="O MUNDO DOS A'S" />
 
-<<<<<<< HEAD
-          {vodeoOpen === false ? (
-=======
-
           {vodeoOpen == false ? (
->>>>>>> origin/main
             <button
               className="rounded-full py-3 px-4 2xl:py-6 2xl:px-9 bg-lightyellow inline-block mt-8 text-xl"
               onClick={openVideoMenu}
@@ -45,7 +40,7 @@ function Header() {
           ) : (
             <div className="fixed top-0 right-0 w-full h-screen bg-white z-20 bg-opacity-95 flex flex-col justify-center items-center">
               <div className="w-8/12" id="video relative">
-                <video  src={mundoAs} controls></video>
+                <video src={mundoAs} controls></video>
                 <span
                   className="cursor-pointer absolute top-5 right-10 text-3xl font-extrabold"
                   onClick={closeVideoMenu}
